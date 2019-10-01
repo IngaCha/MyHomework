ad9621123a9a2f45942a70d9b22c2a219cfde74b
Inga Chamentauskiene <ingamaciunaite@gmail.com>
2019-10-01 14:33:20 +0300

    Hello World program

---
+++ pd_HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Program that prints Hello World! in terminal.
+ * 
+ * @author Inga Chamentauskiene
+ *
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
