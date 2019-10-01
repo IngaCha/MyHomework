4dea4b0219ac61b2aed262cadb4121d56e387de2
Inga Chamentauskiene <ingamaciunaite@gmail.com>
2019-10-01 14:35:29 +0300

    Hello name program

---
+++ pd05/HelloName.java
@@ -0,0 +1,13 @@
+/**
+ * Program that prints Hello + name you write in terminal.
+ * 
+ * @author Inga Chamentauskiene
+ *
+ */
+
+class HelloName {
+	public static void main(String[] name) {
+		// Prints "Hello" + name to the terminal window.
+		System.out.println("Hello " + name[0] + "!");
+	}
+}
